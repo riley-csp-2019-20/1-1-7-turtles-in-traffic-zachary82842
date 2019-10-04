a50a09487845835e1b2ce7282b24a8e57e98789f
zachary82842 <55498296+zachary82842@users.noreply.github.com>
2019-10-04 09:57:02 -0400

    Add files via upload

---
+++ 117ZacharyMcClurken-Kendall12-14.py
@@ -0,0 +1,28 @@
+#   a117_grades.py
+#   This code is incomplete.
+my_courses = ["English", "Math", "CS"]
+
+
+check_grades = True
+while (check_grades):
+    for course in my_courses:
+        print() # blank line
+        print("Enter your points for " + course)
+
+        a = int(input("Points -> "))
+        if (a >= 90):
+	        print("A")
+        elif (a >= 80):
+	        print("B")
+        elif (a >= 70):
+	        print("C")
+        elif (a >= 60):
+	        print("D")
+        else:
+	        print("F")
+            
+    redo = input("Do you need to re-do these grades? (y/n)")
+    if (redo == "y"):
+	    check_grades = True
+    else:
+	    check_grades = False
