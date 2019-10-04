bd51bcfd10eead21a12bc3dfa1bad754d8ad1067
zachary82842 <55498296+zachary82842@users.noreply.github.com>
2019-10-04 09:55:33 -0400

    Add files via upload

--- 117ZacharyMcClurken-Kendall15-19.py
+++ 117ZacharyMcClurken-Kendall15-19.py
@@ -14,6 +14,7 @@ vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown"]
 
 tloc = 50
 for s in turtle_shapes:
+
     # Make the turtles horizontally
 	ht = trtl.Turtle(shape=s)
 	horiz_turtles.append(ht)
@@ -22,6 +23,7 @@ for s in turtle_shapes:
 	ht.fillcolor(new_color)
 	ht.goto(-350, tloc)
 	ht.setheading(0)
+	
     # Make the turtles vertically
 	vt = trtl.Turtle(shape=s)
 	vert_turtles.append(vt)
@@ -34,11 +36,29 @@ for s in turtle_shapes:
 	tloc += 50
 
 # TODO: move turtles across and down screen, stopping for collisions
-"""
+
 steps = 0
 while steps < 50:
 	steps = steps + 1
-"""
+	for h_turtle in horiz_turtles:
+		for v_turtle in vert_turtles:
+
+			v_turtle.forward(10)
+			h_turtle.forward(10)
+
+			x1 = v_turtle.xcor()
+			x2 = h_turtle.xcor()
+
+			y1 = v_turtle.ycor()
+			y2 = h_turtle.ycor()
+
+			if abs(x1 - x2) < 20 and abs(y1 - y2) < 20:
+				v_turtle.fillcolor("grey")
+				h_turtle.fillcolor("grey")
+
+				horiz_turtles.remove(h_turtle)
+				vert_turtles.remove(v_turtle)
+
 
 wn = trtl.Screen()
 wn.mainloop()
